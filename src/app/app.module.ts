6a6d9d452ceb242ee4a888ef2ede1371b0d3cf83
Pun <80048553+thammanant@users.noreply.github.com>
2023-06-13 14:48:12 +0700

    Create custom carouselUI which contain 2 image per view and title with link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CustomCarouselModule } from './custom-carousel/custom-carousel.module';
+import {CustomCarouselComponent} from "./custom-carousel/custom-carousel.component";
+import {CarouselModule} from "primeng/carousel";
+import {TagModule} from "primeng/tag";
+import {ButtonModule} from "primeng/button";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomCarouselComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomCarouselModule,
+    CarouselModule,
+    TagModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
