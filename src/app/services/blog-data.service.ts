8d28a8e64ae8eaa3072abc3bb976edf1565a1ab1
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-06-26 03:51:22 +0700

    createcategory in DB

--- src/app/services/blog-data.service.ts
+++ src/app/services/blog-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { getDatabase, ref, push, set} from "firebase/database";
+import { getDatabase, ref, child, get, set} from "firebase/database";
 import {Blog} from "../model/blog";
 
 @Injectable({
@@ -11,9 +11,30 @@ export class BlogDataService {
   }
 
   writeTest(Blog: Blog) {
-    const db = getDatabase();
-    const PostRef = ref(db, 'blog/' + Blog.id);
-    set(PostRef, Blog).then(r => console.log('success'));
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `blog/${Blog.id}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const db = getDatabase();
+        const PostRef = ref(db, 'blog/' + Blog.id);
+        set(PostRef, Blog).then(r => console.log('success'));
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+        console.error(error);
+      }
+    );
   }
+
+  createCategoryDB(title: string) {
+    const db = getDatabase();
+    const categoryRef = ref(db, 'categories/' + title.toLowerCase().replace(/\s/g, '-'));
+    set(categoryRef, {
+      name: title,
+      key: title.toLowerCase().replace(/\s/g, '-')
+    }).then(r => console.log('success'));
+  }
+
+
 
 }
