fdeaedef52b0eb1e68ccd47ea68f5cca5c38e3ae
Pun <80048553+thammanant@users.noreply.github.com>
2023-06-25 22:44:52 +0700

    finished recent page

--- src/app/recent-view/recent-view.component.ts
+++ src/app/recent-view/recent-view.component.ts
@@ -1,62 +1,33 @@
-// @ts-ignore
-// @ts-ignore
-
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
-
-interface Action {
-  name: string;
-  code: string;
-}
-interface Date {
-  name: string;
-  code: string;
-}
-interface Category {
-  name: string;
-  key: string;
-}
 @Component({
   selector: 'app-recent-view',
   templateUrl: './recent-view.component.html',
   styleUrls: ['./recent-view.component.scss']
 })
-export class RecentViewComponent {
-  search: string = '';
-  // @ts-ignore
-  actions: Action[]
-  // @ts-ignore
-  dates: Date[]
-  // @ts-ignore
-  categories: Category[]
-  // @ts-ignore
-  selectedAction:Action;
-  // @ts-ignore
-  selectedDate: Date;
-  // @ts-ignore
-  selectedCategory: Category;
-
-  recents: any[] = [
-    { title: ' dog with the dragon', author: 'Author1', categories:'categories1', tags: 'tag1', date: 'date', key: 'recent1' },
-    { title: ' elephant eat crocodile', author: 'Author2', categories:'categories2', tags: 'tag2', date: 'date2', key: 'recent2' },
-    { title: ' tfst', author: 'Author3', categories:'categories3', tags: 'tag3', date: 'date3', key: 'recent3' },
+export class RecentViewComponent implements OnInit{
+  bulkAction: any;
+  selectedAction: any;
+  allDates: any;
+  selectedDate: any;
+  allCategories: any;
+  selectedCat: any;
+  selectedBlog: any[] = [];
+  blogs: any[] = [
+    { title: ' Title1', author:' Author1', categories:' cat1',date:' date1', key: 't1' },
+    { title: ' Title2', author:' Author2', categories:' cat2',date:' date2', key: 't2' },
+    { title: ' Title2', author:' Author2', categories:' cat2',date:' date2', key: 't2' },
+    { title: ' Title2', author:' Author2', categories:' cat2',date:' date2', key: 't2' },
   ];
-  selectedRecent: any[] = [];
+  bulkAction2: any;
+  selectedAction2: any;
 
-  ngOnInit()
-  {
-    this.actions = [
-      {name: 'Edit', code: 'Edit'},
-      {name: 'Delete', code: 'Delete'},
-    ];
-
-    this.dates = [
-      {name: 'Month', code: 'Month'},
-    ];
-
-    this.categories = [
-      {name: ' Uncategorized', key: 'Un' },
-    ]
+  formGroup!: FormGroup;
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      checkbox1: new FormControl<string | null>(null)
+    });
   }
 }
 
