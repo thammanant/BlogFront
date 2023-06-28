840eb3cba53ceae8bc22e97ccedc7c6c6c69de0b
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-06-28 10:25:20 +0700

    create environment file for DB config import firebaseConfig from environment instead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,19 +18,10 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB4eLDyg7r6SdkSL5J4SewsmfS6FoLre7s",
-  authDomain: "bestblog-51d5a.firebaseapp.com",
-  databaseURL: "https://bestblog-51d5a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bestblog-51d5a",
-  storageBucket: "bestblog-51d5a.appspot.com",
-  messagingSenderId: "292913735225",
-  appId: "1:292913735225:web:bd64693b97e61b2cd2cfe8",
-  measurementId: "G-XV786L6SPC"
-}
+import {environment} from "src/environments/environment";
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyD-5Z4Z3Q4Z3Q4Z3Q4Z3Q4Z3Q4Z3Q4Z3Q4",
+        authDomain: "bestblog-51d5a.firebaseapp.com",
+        databaseURL: "https://bestblog-51d5a-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "bestblog-51d5a",
+        storageBucket: "bestblog-51d5a.appspot.com",
+        messagingSenderId: "292913735225",
+        appId: "1:292913735225:web:bd64693b97e61b2cd2cfe8",
+        measurementId: "G-XV786L6SPC"
+    }
+}
